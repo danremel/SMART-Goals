2ed1cff17ed3e486bc976dcea49f9d7510e3024c
danremel <remel.dan@gmail.com>
2019-04-15 11:07:03 -0400

    Added bootstrap to Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { GoalListComponent } from './goal-list/goal-list.component';
+import { GoalAddComponent } from './goal-list/goal-add/goal-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    GoalListComponent,
+    GoalAddComponent
   ],
   imports: [
     BrowserModule,
